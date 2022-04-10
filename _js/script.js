4db34412396e38c03a1c031918515b25f73a9258
Mateus Tomas Lins <tommattih@gmail.com>
2022-04-09 21:47:10 -0300

    mobile run

--- _js/script.js
+++ _js/script.js
@@ -1,11 +1,20 @@
-//Preloader
+////////////Preloader
 const preloader = document.querySelectorAll('#preloader');
 
 function preload() {
   document.getElementById("preloader").classList.add('hidden');
 }
 
-//menu
-// function close(){
-//   document.getElementById("close").classList.add('hidden')
-// }
+////////////menu
+function openClose(){
+let menuMobile = document.querySelector("#menu");
+menuMobile
+  if (menuMobile.classList.contains('hidden') == true){
+    menuMobile.classList.remove('hidden');
+    alert.log("mostrou ^^");
+
+  }else{
+    menuMobile.classList.add('hidden');
+    alert.log("escondeu !!! ^^");
+  }
+}
